0821a4e301971e0c16a62bcefa5df0e860dd7d05
Grazielle Bastos <grazielle1101@gmail.com>
2025-03-19 23:30:38 -0300

    Criação do Projeto

---
+++ formacaoJavaWeb/src/cursoJava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,14 @@
+package cursoJava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main é um método auto executável em 
+	 Java*/
+	public static void main(String[] args) {
+		
+		/*Imprime a mensagem no console | Escreve o texto no console*/
+		System.out.println("Meu primeiro programa em Java do curso 'Formação Java Web', JDEV Treinamento");
+		System.out.println("\nJava é orientado a objetos");
+	}
+
+}

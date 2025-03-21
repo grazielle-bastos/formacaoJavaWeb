6e1276e8b8cf6e9459824fd459e45193a0c3a6c6
Grazielle Bastos <grazielle1101@gmail.com>
2025-03-20 23:27:16 -0300

    Adicionando o campo de idade na classe Pessoa

--- formacaoJavaWeb/src/treinoGit/jdev/Pessoa.java
+++ formacaoJavaWeb/src/treinoGit/jdev/Pessoa.java
@@ -6,6 +6,8 @@ public class Pessoa {
 	private String nome;
 	
 	private String sobrenome;
+	
+	private int idade;
 
 	public String getNome() {
 		return nome;
@@ -21,6 +23,14 @@ public class Pessoa {
 
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
 	}
 
 }

3a1d28ea7ca3fd20aa89d2747626344894624ca1
Grazielle Bastos <grazielle1101@gmail.com>
2025-03-22 15:21:54 -0300

    Código da tarefa 1, branch 1

--- formacaoJavaWeb/src/cursoJava/executavel/PrimeiraClasseJava.java
+++ formacaoJavaWeb/src/cursoJava/executavel/PrimeiraClasseJava.java
@@ -1,14 +1,19 @@
 package cursoJava.executavel;
 
 public class PrimeiraClasseJava {
-	
-	/*Main é um método auto executável em 
-	 Java*/
+
 	public static void main(String[] args) {
-		
-		/*Imprime a mensagem no console | Escreve o texto no console*/
+
+		/*
+		 * Main é um método auto executável em Java
+		 */
+
+		String linguagem = "Java";
+
+		/* Imprime a mensagem no console | Escreve o texto no console */
 		System.out.println("Meu primeiro programa em Java do curso 'Formação Java Web', JDEV Treinamento");
-		System.out.println("\n\nJava é orientado a objetos");
+		System.out.println("\n\n" + linguagem + " é orientado a objetos");
+
 	}
 
 }
